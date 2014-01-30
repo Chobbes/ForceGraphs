98b9181b7b84a500617d078cbc0f3f115cfc2729
Calvin Beck <hobbes@ualberta.ca>
2014-01-29 23:09:25 -0700

    type, not newtype. Added a position type synonym.

--- forces.hs
+++ forces.hs
@@ -1,12 +1,15 @@
+import Control.Arrow
+
 -- Each vertex must have an associated "charge" for repulsion.
 -- Each edge must have a constant for springiness.
 
 data Node = Node { charge :: Double
-                 , position :: (Double, Double)
+                 , position :: Position
                  } deriving (Equals, Show)
 
 data Spring = Spring { stiffness :: Double
                      , nodes :: (Node, Node)
                      } deriving (Equals, Show)
 
-newtype Force = (Double, Double)
+type Force = (Double, Double)
+type Position = (Double, Double)

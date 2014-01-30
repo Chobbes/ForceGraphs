5bcd4866752c533603f56207c7c2cb074687a2f3
Calvin Beck <hobbes@ualberta.ca>
2014-01-29 23:23:21 -0700

    Normalization function added.

--- forces.hs
+++ forces.hs
@@ -14,9 +14,11 @@ data Spring = Spring { stiffness :: Double
 type Force = (Double, Double)
 type Position = (Double, Double)
 
+-- Get the magnitude of a tuple (vector magnitude).
 magnitude :: (Double, Double) -> Double
 magnitude = sqrt . tupleSum . ((**2) *** (**2))
   where tupleSum t = fst t + snd t
 
-
-
+-- Returns a tuple with a magnitude of 1 (barring floating point errors).
+normalize :: (Double, Double) -> (Double, Double)
+normalize t@(x, y) = let mag = magnitude t in (x / mag, y / mag)
